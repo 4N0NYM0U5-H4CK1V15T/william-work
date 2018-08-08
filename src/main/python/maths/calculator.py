9629ff290c2f16671e2315acdf36121a38d60b64
4NONYM0U5-H4CK1v15T <williame15254@gmail.com>
2018-08-08 15:05:45 +0100

    Added calculator.py

---
+++ src/main/python/maths/calculator.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  8 14:49:32 2018
+
+@author: William
+"""
+
+from maths.Parser import Parser
+
+while True:
+    userInput = input("Input: ")
+    exp =  Parser.parse(userInput)
+    print(str(exp) + " = " + str(exp.evaluate()))
